9fe9ea190f2be272d9f6114ea253691550883247
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2024-03-22 08:54:44 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,38 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
+
+    name='''ckanext-thesauri''',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.0.1',
+
+    description='''''',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # The project's main homepage.
+    url='https://github.com/cuprit-gbr/ckanext-thesauri',
+
+    # Author details
+    author='''cuprit-gbr''',
+    author_email='''toni.schoenbuchner@cuprit.net''',
+
+    # Choose your license
+    license='AGPL',
+
     # If you are changing from the default layout of your extension, you may
     # have to change the message extractors, you can read more about babel
     # message extraction at
